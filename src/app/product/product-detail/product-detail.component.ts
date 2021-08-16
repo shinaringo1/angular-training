ea076c855b41df3775e71921630188b3d4bfc373
shinaringo1 <kudo.darts@gmail.com>
2021-08-16 16:12:14 +0900

    81-product-detail-etc

---
+++ src/app/product/product-detail/product-detail.component.ts
@@ -0,0 +1,22 @@
+import { parseI18nMeta } from '@angular/compiler/src/render3/view/i18n/meta';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from 'src/app/products';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit {
+  product: any;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get('productId')!]
+    })
+  }
+
+}
---
+++ src/app/products.ts
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    heading1: 'heading 1 XL',
+    heading2: 'heading 2 XL',
+    heading3: 'heading 3 XL',
+    headingtext1: 'This is a sample text for heading 1',
+    headingtext2: 'This is a sample text for heading 2',
+    headingtext3: 'This is a sample text for heading 3'
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    heading1: 'heading 1 Mini',
+    heading2: 'heading 2 Mini',
+    heading3: 'heading 3 Mini',
+    headingtext1: 'This is a sample text for heading 1',
+    headingtext2: 'This is a sample text for heading 2',
+    headingtext3: 'This is a sample text for heading 3'
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone Standard',
+    price: 299,
+    description: 'NA',
+    heading1: 'heading 1 Standard',
+    heading2: 'heading 2 Standard',
+    heading3: 'heading 3 Standard',
+    headingtext1: 'This is a sample text for heading 1',
+    headingtext2: 'This is a sample text for heading 2',
+    headingtext3: 'This is a sample text for heading 3'
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone Special',
+    price: 999,
+    description: 'NA',
+    heading1: 'heading 1 Special',
+    heading2: 'heading 2 Special',
+    heading3: 'heading 3 Special',
+    headingtext1: 'This is a sample text for heading 1',
+    headingtext2: 'This is a sample text for heading 2',
+    headingtext3: 'This is a sample text for heading 3'
+  }
+];
